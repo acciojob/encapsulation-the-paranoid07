329ecb348777110a40ec04bbd58fc51bd35befb5
Anish <anishpal9621@gmail.com>
2023-03-27 11:55:29 +0530

    Initia attempt of assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
+import com.sun.security.jgss.GSSUtil;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj =new RWOnly();
+
+        //System.out.println(obj.name="manu");
+
+        obj.setName("Manu");
+
+        System.out.println(obj.getName());
+    }
 }
